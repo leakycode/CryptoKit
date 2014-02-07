6f30dcfc62e576cc59115f4cc12e997c91ad3da5
Mehrdad Afshari <git@id.mehrdada.com>
2014-02-06 18:02:35 -0800

    Updated CKCrypto.h to import AES in GCM mode

--- CKCrypto.h
+++ CKCrypto.h
@@ -1,8 +1,3 @@
-#import "CKData.h"
-#import "CKSensitiveBuffer.h"
-#import "CKMessageDigest.h"
-#import "CKDigitalSignature.h"
-#import "CKKeyExchange.h"
-
 #import "CKSHA.h"
 #import "CKEd25519.h"
+#import "CKGCMAES.h"
